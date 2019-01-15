584514738758fb3da89f75b02ae003844fe2d622
Isha Niroula <ishuniroula1999@gmail.com>
2019-01-15 10:21:06 +0545

    my first commit

---
+++ guessinggame.py
@@ -0,0 +1,15 @@
+name = input("Enter your name: ")
+what_you_wanna_be = input("Enter what you wanna be in future: ")
+color = input("Enter your favourite color: ")
+hotel = input("Enter a place where you won't have dinner: ")
+food = input("Enter the food that you will never eat in your life: ")
+place = input("Enter where you wanna have your future home: ")
+your_hatest_animal = input("Enter a non-veg dish that you wanna have now: ")
+dish = input("Enter a vegeterian dish: ")
+
+print(" Welcome " + name + " to the future world!!!  "),
+print("  You will be living in " + place + "."),
+print("   Your occupation will be " + what_you_wanna_be + "."),
+print("    You will have a car of " + color + " color."),
+print("     You will go to " + hotel + " and order " + food + "."),
+print("      You will never eat " + your_hatest_animal + " because you will love " + dish + ".")
